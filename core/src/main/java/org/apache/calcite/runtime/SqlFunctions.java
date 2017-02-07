d0362c878574fe6df42ba298a25feecf48c71111
Minji Kim <minji@dremio.com>
2017-02-06 16:32:01 -0800

    Fix missing power function
    Change-Id: I948e252c0cf299f9a184a6f16cfdc42101906213

--- core/src/main/java/org/apache/calcite/runtime/SqlFunctions.java
+++ core/src/main/java/org/apache/calcite/runtime/SqlFunctions.java
@@ -837,6 +837,10 @@ public class SqlFunctions {
     return Math.pow(b0.doubleValue(), b1.doubleValue());
   }
 
+  public static double power(BigDecimal b0, long b1) {
+    return Math.pow(b0.doubleValue(), b1);
+  }
+
   public static double power(long b0, BigDecimal b1) {
     return Math.pow(b0, b1.doubleValue());
   }
