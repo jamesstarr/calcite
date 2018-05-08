9c5b3cec6976255b5cb69f21d0d4637dfba9f351
Julian Hyde <jhyde@apache.org>
2018-05-08 10:23:10 -0700

    [CALCITE-2304] In Babel parser, allow Hive-style syntax "LEFT SEMI JOIN"
    Change-Id: Ieb852884b104d972debb3277b43ef2dc40a52218

--- core/src/main/java/org/apache/calcite/sql/validate/SqlValidatorImpl.java
+++ core/src/main/java/org/apache/calcite/sql/validate/SqlValidatorImpl.java
@@ -3101,6 +3101,12 @@ public class SqlValidatorImpl implements SqlValidatorWithHints {
     // Which join types require/allow a ON/USING condition, or allow
     // a NATURAL keyword?
     switch (joinType) {
+    case LEFT_SEMI_JOIN:
+      if (!conformance.isLiberal()) {
+        throw newValidationError(join.getJoinTypeNode(),
+            RESOURCE.dialectDoesNotSupportFeature("LEFT SEMI JOIN"));
+      }
+      // fall through
     case INNER:
     case LEFT:
     case RIGHT:
