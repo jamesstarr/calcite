9a182769c3b7c763bdf5515c087b9321283e521c
Laurent Goujon <laurent@dremio.com>
2019-07-26 04:25:39 +0000

    Revert "[CALCITE-3005] Implement string functions: LEFT, RIGHT (xuqianjin)"
    It shouldn't have been merged until Dremio changes are ready. Causing one test failure, and blocking another patch to be merged, so temporarily reverting
    
    This reverts commit f44c1beafa13f50adf4db05e9bd64220057522e9.
    
    Change-Id: Iea9965da0c288fa2ae6fc87247d7e9ba3b9da8ba

--- core/src/main/java/org/apache/calcite/runtime/SqlFunctions.java
+++ core/src/main/java/org/apache/calcite/runtime/SqlFunctions.java
@@ -288,6 +288,9 @@ public class SqlFunctions {
 
   /** SQL {@code OVERLAY} function. */
   public static String overlay(String s, String r, int start) {
+    if (s == null || r == null) {
+      return null;
+    }
     return s.substring(0, start - 1)
         + r
         + s.substring(start - 1 + r.length());
@@ -295,6 +298,9 @@ public class SqlFunctions {
 
   /** SQL {@code OVERLAY} function. */
   public static String overlay(String s, String r, int start, int length) {
+    if (s == null || r == null) {
+      return null;
+    }
     return s.substring(0, start - 1)
         + r
         + s.substring(start - 1 + length);
@@ -302,6 +308,9 @@ public class SqlFunctions {
 
   /** SQL {@code OVERLAY} function applied to binary strings. */
   public static ByteString overlay(ByteString s, ByteString r, int start) {
+    if (s == null || r == null) {
+      return null;
+    }
     return s.substring(0, start - 1)
            .concat(r)
            .concat(s.substring(start - 1 + r.length()));
@@ -310,6 +319,9 @@ public class SqlFunctions {
   /** SQL {@code OVERLAY} function applied to binary strings. */
   public static ByteString overlay(ByteString s, ByteString r, int start,
       int length) {
+    if (s == null || r == null) {
+      return null;
+    }
     return s.substring(0, start - 1)
            .concat(r)
            .concat(s.substring(start - 1 + length));
